eb82561e72bc13547de621abaa0b1cb6a40badac
Aiden Cullo <aidencullo@gmail.com>
2024-08-30 16:41:59 -0400

    extract route handlers to functions

--- index.js
+++ index.js
@@ -5,28 +5,37 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/goodbye') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1 style="color: blue">Goodbye World!</h1>');
-    res.write('<p>Thanks for visiting!</p>');
-    res.end();
-    return;
+    sayGoodbye(res);
   } else if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1 style="color: red">Hello World!</h1>');
-    res.write('<p>I wonder what else we can send...</p>');
-    res.end();
-    return;
+    sayHello(res);
   } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1 style="color: red">404 Not Found</h1>');
-    res.write('<p>Sorry, we can\'t find that page.</p>');
-    res.end();
-    return;
+    notFound(res);
   }
 });
+
+const sayGoodbye = (res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1 style="color: blue">Goodbye World!</h1>');
+  res.write('<p>Thanks for visiting!</p>');
+  res.end();
+};
+
+const sayHello = (res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1 style="color: green">Hello World!</h1>');
+  res.write('<p>Welcome to my website!</p>');
+  res.end();
+};
+
+const notFound = (res) => {
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1 style="color: red">404 Not Found</h1>');
+  res.write('<p>Sorry, the page you are looking for is not here.</p>');
+  res.end();
+};
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
